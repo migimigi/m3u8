c244d5e1dd7beeb2ce890eebd8defb49ff0d11ee
migimigi <migimigi@users.noreply.github.com>
2021-06-25 19:12:30 +0900

    Remove warning for atom
    Example
    warning: found quoted atom "download" but the quotes are not required. Atoms made exclusively of ASCII letters, numbers, underscores, and optionally ending with ! or ? do not require quotes
      lib/M3U8.ex:6

--- lib/M3U8.ex
+++ lib/M3U8.ex
@@ -3,112 +3,112 @@
 # Using rebar3_elixir (https://github.com/G-Corp/rebar3_elixir)
 # MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
 defmodule M3U8 do
-  def unquote(:"download")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"download", [arg1, arg2])
+  def unquote(:download)(arg1, arg2) do
+    :erlang.apply(:m3u8, :download, [arg1, arg2])
   end
-  def unquote(:"validate")(arg1) do
-    :erlang.apply(:"m3u8", :"validate", [arg1])
+  def unquote(:validate)(arg1) do
+    :erlang.apply(:m3u8, :validate, [arg1])
   end
-  def unquote(:"validate")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"validate", [arg1, arg2])
+  def unquote(:validate)(arg1, arg2) do
+    :erlang.apply(:m3u8, :validate, [arg1, arg2])
   end
-  def unquote(:"audio_codec_code")(arg1) do
-    :erlang.apply(:"m3u8", :"audio_codec_code", [arg1])
+  def unquote(:audio_codec_code)(arg1) do
+    :erlang.apply(:m3u8, :audio_codec_code, [arg1])
   end
-  def unquote(:"video_codec_code")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"video_codec_code", [arg1, arg2])
+  def unquote(:video_codec_code)(arg1, arg2) do
+    :erlang.apply(:m3u8, :video_codec_code, [arg1, arg2])
   end
-  def unquote(:"parse")(arg1) do
-    :erlang.apply(:"m3u8", :"parse", [arg1])
+  def unquote(:parse)(arg1) do
+    :erlang.apply(:m3u8, :parse, [arg1])
   end
-  def unquote(:"to_binary")(arg1) do
-    :erlang.apply(:"m3u8", :"to_binary", [arg1])
+  def unquote(:to_binary)(arg1) do
+    :erlang.apply(:m3u8, :to_binary, [arg1])
   end
-  def unquote(:"to_file")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"to_file", [arg1, arg2])
+  def unquote(:to_file)(arg1, arg2) do
+    :erlang.apply(:m3u8, :to_file, [arg1, arg2])
   end
-  def unquote(:"new")() do
-    :erlang.apply(:"m3u8", :"new", [])
+  def unquote(:new)() do
+    :erlang.apply(:m3u8, :new, [])
   end
-  def unquote(:"version")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"version", [arg1, arg2])
+  def unquote(:version)(arg1, arg2) do
+    :erlang.apply(:m3u8, :version, [arg1, arg2])
   end
-  def unquote(:"version")(arg1) do
-    :erlang.apply(:"m3u8", :"version", [arg1])
+  def unquote(:version)(arg1) do
+    :erlang.apply(:m3u8, :version, [arg1])
   end
-  def unquote(:"target_duration")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"target_duration", [arg1, arg2])
+  def unquote(:target_duration)(arg1, arg2) do
+    :erlang.apply(:m3u8, :target_duration, [arg1, arg2])
   end
-  def unquote(:"target_duration")(arg1) do
-    :erlang.apply(:"m3u8", :"target_duration", [arg1])
+  def unquote(:target_duration)(arg1) do
+    :erlang.apply(:m3u8, :target_duration, [arg1])
   end
-  def unquote(:"playlist_type")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"playlist_type", [arg1, arg2])
+  def unquote(:playlist_type)(arg1, arg2) do
+    :erlang.apply(:m3u8, :playlist_type, [arg1, arg2])
   end
-  def unquote(:"playlist_type")(arg1) do
-    :erlang.apply(:"m3u8", :"playlist_type", [arg1])
+  def unquote(:playlist_type)(arg1) do
+    :erlang.apply(:m3u8, :playlist_type, [arg1])
   end
-  def unquote(:"media_sequence")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"media_sequence", [arg1, arg2])
+  def unquote(:media_sequence)(arg1, arg2) do
+    :erlang.apply(:m3u8, :media_sequence, [arg1, arg2])
   end
-  def unquote(:"media_sequence")(arg1) do
-    :erlang.apply(:"m3u8", :"media_sequence", [arg1])
+  def unquote(:media_sequence)(arg1) do
+    :erlang.apply(:m3u8, :media_sequence, [arg1])
   end
-  def unquote(:"program_datetime")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"program_datetime", [arg1, arg2])
+  def unquote(:program_datetime)(arg1, arg2) do
+    :erlang.apply(:m3u8, :program_datetime, [arg1, arg2])
   end
-  def unquote(:"program_datetime")(arg1) do
-    :erlang.apply(:"m3u8", :"program_datetime", [arg1])
+  def unquote(:program_datetime)(arg1) do
+    :erlang.apply(:m3u8, :program_datetime, [arg1])
   end
-  def unquote(:"allow_cache")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"allow_cache", [arg1, arg2])
+  def unquote(:allow_cache)(arg1, arg2) do
+    :erlang.apply(:m3u8, :allow_cache, [arg1, arg2])
   end
-  def unquote(:"allow_cache")(arg1) do
-    :erlang.apply(:"m3u8", :"allow_cache", [arg1])
+  def unquote(:allow_cache)(arg1) do
+    :erlang.apply(:m3u8, :allow_cache, [arg1])
   end
-  def unquote(:"iframe_only")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"iframe_only", [arg1, arg2])
+  def unquote(:iframe_only)(arg1, arg2) do
+    :erlang.apply(:m3u8, :iframe_only, [arg1, arg2])
   end
-  def unquote(:"iframe_only")(arg1) do
-    :erlang.apply(:"m3u8", :"iframe_only", [arg1])
+  def unquote(:iframe_only)(arg1) do
+    :erlang.apply(:m3u8, :iframe_only, [arg1])
   end
-  def unquote(:"segment")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"segment", [arg1, arg2])
+  def unquote(:segment)(arg1, arg2) do
+    :erlang.apply(:m3u8, :segment, [arg1, arg2])
   end
-  def unquote(:"segments")(arg1) do
-    :erlang.apply(:"m3u8", :"segments", [arg1])
+  def unquote(:segments)(arg1) do
+    :erlang.apply(:m3u8, :segments, [arg1])
   end
-  def unquote(:"segments")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"segments", [arg1, arg2])
+  def unquote(:segments)(arg1, arg2) do
+    :erlang.apply(:m3u8, :segments, [arg1, arg2])
   end
-  def unquote(:"key")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"key", [arg1, arg2])
+  def unquote(:key)(arg1, arg2) do
+    :erlang.apply(:m3u8, :key, [arg1, arg2])
   end
-  def unquote(:"keys")(arg1) do
-    :erlang.apply(:"m3u8", :"keys", [arg1])
+  def unquote(:keys)(arg1) do
+    :erlang.apply(:m3u8, :keys, [arg1])
   end
-  def unquote(:"keys")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"keys", [arg1, arg2])
+  def unquote(:keys)(arg1, arg2) do
+    :erlang.apply(:m3u8, :keys, [arg1, arg2])
   end
-  def unquote(:"media")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"media", [arg1, arg2])
+  def unquote(:media)(arg1, arg2) do
+    :erlang.apply(:m3u8, :media, [arg1, arg2])
   end
-  def unquote(:"medias")(arg1) do
-    :erlang.apply(:"m3u8", :"medias", [arg1])
+  def unquote(:medias)(arg1) do
+    :erlang.apply(:m3u8, :medias, [arg1])
   end
-  def unquote(:"medias")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"medias", [arg1, arg2])
+  def unquote(:medias)(arg1, arg2) do
+    :erlang.apply(:m3u8, :medias, [arg1, arg2])
   end
-  def unquote(:"playlist")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"playlist", [arg1, arg2])
+  def unquote(:playlist)(arg1, arg2) do
+    :erlang.apply(:m3u8, :playlist, [arg1, arg2])
   end
-  def unquote(:"playlists")(arg1) do
-    :erlang.apply(:"m3u8", :"playlists", [arg1])
+  def unquote(:playlists)(arg1) do
+    :erlang.apply(:m3u8, :playlists, [arg1])
   end
-  def unquote(:"playlists")(arg1, arg2) do
-    :erlang.apply(:"m3u8", :"playlists", [arg1, arg2])
+  def unquote(:playlists)(arg1, arg2) do
+    :erlang.apply(:m3u8, :playlists, [arg1, arg2])
   end
-  def unquote(:"discontinuity")(arg1) do
-    :erlang.apply(:"m3u8", :"discontinuity", [arg1])
+  def unquote(:discontinuity)(arg1) do
+    :erlang.apply(:m3u8, :discontinuity, [arg1])
   end
 end
